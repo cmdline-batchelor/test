04d61fa75b3ec4cdc2eb59625717c845975692d1
Dionysus <dionysus@localhost.localdomain>
2020-09-30 00:56:57 +0100

    closer

--- roles/viYum/files/pythVim.sh
+++ roles/viYum/files/pythVim.sh
@@ -1,11 +1,9 @@
-   cd ~
-   cd vim
+   cd ~/vim
 
-   ./configure  --with-features=huge                                       \
-   --enable multibyte                                                      \
-   --enable-rubyinterp                                                     \
-   --enable-python3interp                                                  \
-   --with-python3-config-dir=/lib64/python3.6/config-3.6m-x86_64-linux-gnu \
+   ./configure  --with-features=huge                                           \
+   --enable-python3interp=yes                                                  \
+   --enable-multibyte=yes                                                      \
+   --enable-luainterp=yes                                                      \
+   --with-python3-config-dir=/lib64/python3.6/config-3.6m-x86_64-linux-gnu     \
 
    make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
-
